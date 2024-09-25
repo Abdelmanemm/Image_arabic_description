742d3e24d8e99c23387ee3d6883a7415d2a2e90c
Abdelmanem Mohamed <113713970+Abdelmanemm@users.noreply.github.com>
2024-09-25 14:05:27 +0300

    Update APP.py

--- App/APP.py
+++ App/APP.py
@@ -3,8 +3,11 @@ from Translate_en_to_ar import Translate_en_to_ar
 from TextToAudio import TextToAudio
 import gradio as gr
 
-
+# Define App Class 
 class APP:
+    ''' Take an Image an return speech descibe image in arabic 
+        When Class object is created it intialize all the 3 classes 
+        Run which is our pipeline,take an image and return audio '''
     def __init__(self):
         self.image_captioner  = ImageCaption()
         self.text_to_audio = TextToAudio()
@@ -16,13 +19,14 @@ class APP:
         audio = self.text_to_audio.get_audio(translation,'image_descripton.mp3')
         return audio
     
-
+# APP object which will be used in the interface 
 app = APP()
 
+# Creating Gradio interface to interact and use or pipeline in high level
 iface = gr.Interface(
     fn=app.Run,
-    inputs=gr.Image(type="numpy"),  # Accepts an image
-    outputs=gr.Audio(type="filepath"),  # Outputs an audio file
+    inputs=gr.Image(type="numpy"),  
+    outputs=gr.Audio(type="filepath"),  
     title="Image Caption Translator",
     description="Upload an image to generate an Arabic audio description.",
 )
